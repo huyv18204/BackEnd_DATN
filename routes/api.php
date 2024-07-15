a9b589abb2f70e2858b429af512bd5048ea81728
quanlhph39312 <quanlhph39312@fpt.edu.vn>
2024-07-15 23:50:21 +0700

    feature/product_variant_category

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductVariantController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,11 +23,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::prefix("v1")->group(function () {
+    Route::prefix('category')->group(function () {
+        Route::get('', [CategoryController::class, 'index']);
+        Route::post('', [CategoryController::class, 'store']);
+        Route::put('/{id}', [CategoryController::class, 'update']);
+        Route::delete('/{id}', [CategoryController::class, 'destroy']);
+    });
     Route::prefix("products")->group(function () {
         Route::get("/", [ProductController::class, 'index']);
         Route::post("/", [ProductController::class, 'store']);
         Route::put("/{id}", [ProductController::class, 'update']);
         Route::get("/{id}", [ProductController::class, 'show']);
         Route::delete("/{id}", [ProductController::class, 'destroy']);
+    });
+    Route::prefix('products/{id}')->group(function () {
+        Route::get('productVariant', [ProductVariantController::class, 'show']);
+        Route::post('productVariant', [ProductVariantController::class, 'store']);
+        Route::put('productVariant/{variant_id}', [ProductVariantController::class, 'update']);
+        Route::delete('productVariant/{variant_id}', [ProductVariantController::class, 'destroy']);
     });
 });
